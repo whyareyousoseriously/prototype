a501972d265c5a9841a0d53582056170623613c5
whyareyousoseriously <cz826033956@126.com>
2017-11-17 00:00:52 +0800

    maven-learn-ongoing

---
+++ hongxing-bge/src/main/java/com/imooc/hongxing/hongxing_bge/App.java
@@ -0,0 +1,13 @@
+package com.imooc.hongxing.hongxing_bge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
